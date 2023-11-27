f39e0d631a18ed2923c089740acfd11a4d5dab94
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-27 11:37:00 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <Image src="/logo/logoText.jpg" alt="logo" width={150} height={150} />
-      <h1 className='my-10 text-4xl text-gray-700'>Site en construction</h1>
+      <h1 className='my-10 text-4xl text-gray-700 text-center'>Site en construction</h1>
       <div className='p-3'>
-        <h2>Contactez-nous:</h2>
+        <h2 className='text-center'>Contactez-nous:</h2>
         <div className='flex w-full justify-around mt-2'>
           <a href="mailto:s.olivar@chateaudutruch.com">
             <EnvelopeIcon className="w-6 h-6 hover:scale-110 transition-all duration-150 ease-out" />
